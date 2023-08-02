6a12bfdeb28fabb2dc1dfec969f0e0f0c7639f15
rjtmehta99 <rjtmehta99@gmail.com>
2023-08-02 23:49:15 +0200

    combined retriever, reader, QG. Improved dashboard for meeting

---
+++ constants.py
@@ -0,0 +1,3 @@
+css = 'footer {visibility: hidden}'
+tab_title = 'MSc Thesis - Rajat Mehta'
+page_title = '<div style="text-align: center; margin: 0 auto;"><h1 style="font-size: 40px;">Beyond Handcrafting Screening Questions<br></h1></div>'
--- gradio_demo.py
+++ gradio_demo.py
@@ -1,9 +1,19 @@
 import gradio as gr
 import pandas as pd
 import helper
-from haystack.nodes import QuestionGenerator, FARMReader
+import constants
+from haystack.nodes import QuestionGenerator, FARMReader, BM25Retriever
 from haystack.pipelines import QuestionAnswerGenerationPipeline
 from haystack.document_stores import ElasticsearchDocumentStore
+
+question_generator = QuestionGenerator(model_name_or_path='valhalla/t5-base-e2e-qg',
+                                        max_length=420, split_length=75, 
+                                        split_overlap=20, use_gpu=True)
+reader = FARMReader(model_name_or_path='deepset/roberta-base-squad2',
+                    top_k=1, use_gpu=True)
+pipeline = QuestionAnswerGenerationPipeline(question_generator, reader)
+
+theme = gr.themes.Soft()
 
 def upload_csv(topic, file, labels, data_source):
     print('Uploading')
@@ -15,7 +25,7 @@ def upload_csv(topic, file, labels, data_source):
     doc_store = helper.add_to_docstore(docs, index=topic, delete_docs=True)
     if labels != '':
         labels = labels.split(',')
-        labels = [label.strip for label in labels]
+        labels = [label.strip() for label in labels]
         doc_store = helper.classify_docs(labels=labels, doc_store=doc_store, index=topic)
         return [f'CSV added to Elasticsearch under {topic}.\nLabels added.', 
                 gr.update(visible=True),
@@ -23,24 +33,25 @@ def upload_csv(topic, file, labels, data_source):
 
     return [f'CSV added to Elasticsearch under {topic}.', 
             gr.update(visible=True),
+            gr.update(visible=True),
             gr.update(visible=True)]
 
 
-def generate_qa_pairs(topic):
-    question_generator = QuestionGenerator(model_name_or_path='valhalla/t5-base-e2e-qg',
-                                       max_length=420, split_length=75, 
-                                       split_overlap=20, use_gpu=True)
-    reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2", 
-                    top_k=1, use_gpu=True)
-    pipeline = QuestionAnswerGenerationPipeline(question_generator, reader)
+def generate_qa_pairs(topic, retrieval_query):
     doc_store = ElasticsearchDocumentStore(index=topic)
-    docs = doc_store.get_all_documents()
-    results = pipeline.run(documents=docs)
-
+    if retrieval_query == '':
+        retrieval_flag = False
+        docs = doc_store.get_all_documents()
+    else:
+        retrieval_flag = True
+        retriever = BM25Retriever(document_store=doc_store)
+        docs = retriever.retrieve(query=retrieval_query)
+        
     generated_ques = []
     generated_ans = []
     doc_contexts = []
 
+    results = pipeline.run(documents=docs)
     for query_content, answer_content, document_content in zip(results['queries'], results['answers'], results['documents']):
         answer = answer_content[0]
         document = document_content[0]
@@ -49,31 +60,62 @@ def generate_qa_pairs(topic):
             generated_ans.append(answer.answer)
             doc_contexts.append(document.content)
 
-    df_gen_qa = pd.DataFrame(data={'generated_question':generated_ques, 'generated_answer':generated_ans,
-                                'document_context':doc_contexts})
-    df_gen_qa.to_csv(f'data/{topic}_generated_QA.csv', index=False)
+    df_gen_qa = pd.DataFrame(data={'generated_question':generated_ques, 'generated_answer':generated_ans, 'document_context':doc_contexts})
+    path = f'data/{topic}_generated_QA.csv'
+    df_gen_qa.to_csv(path, index=False)
+
+    if retrieval_flag:
+        qa_output_str = f'{len(df_gen_qa)} QA pairs were generated using documents filtered on "{retrieval_query}". Download the file below.'
+    else:
+        qa_output_str = f'{len(df_gen_qa)} QA pairs were generated. Download the file below.'
+
+    return [gr.update(value=qa_output_str, visible=True),
+            gr.update(value=path, visible=True),
+            gr.update(value=df_gen_qa, visible=True)]
 
 
-with gr.Blocks() as demo:
+def change_label(topic):
+    return gr.update(label=f'Add {topic} CSV')
+
+with gr.Blocks(css=constants.css, title=constants.tab_title, theme=theme) as demo:
+    gr.HTML(constants.page_title)
     with gr.Row():
-        topic = gr.Textbox(label='Topic', placeholder='openstax_biology, ck12_economics...')
-        data_source = gr.Dropdown(["OpenStax.org", "CK12.org", "Brightstorm"], label="Data Source")
-        labels = gr.Textbox(label='Zero Shot Labels', placeholder= 'Add labels science, economics, technology...(use , to split)')
-    file = gr.File(file_types=['csv'], label='Add CSV for topic')
+        topic = gr.Textbox(label='Topic', 
+                           placeholder='openstax_biology, ck12_economics...')
+        data_source = gr.Dropdown(['OpenStax.org', 'CK12.org', 'Brightstorm'],
+                                  label='Data Source')
+        labels = gr.Textbox(label='Zero Shot Labels', 
+                            placeholder= 'Add labels science, economics, technology...(use , to split)')
+    
+    file = gr.File(file_types=['csv'], label='Add CSV')
+    #topic.change(fn=lambda x: file.update(label=f'Add CSV file for {topic.value}'), inputs=topic, outputs=file)
+    topic.change(fn=change_label, inputs=topic, outputs=file)
+    
     upload_btn = gr.Button('Upload')
-    output_box = gr.Textbox(label='', value='', visible=False)
-    generate_qa_btn = gr.Button(f'Generate {topic} Question Answer Pairs', visible=False)
-    upload_btn.click(fn=upload_csv, inputs=[topic, file, labels, data_source], outputs=[output_box, output_box, generate_qa_btn])
+    data_output_box = gr.Textbox(label='Data Upload Status', visible=False)
+    retrieval_query = gr.Textbox(label='Retrieval Query - BM25', placeholder='Enter query', visible=False)
+    generate_qa_btn = gr.Button(f'Generate Question Answer Pairs', visible=False)
 
-    generate_qa_btn.click(fn=generate_qa_pairs, inputs=[topic], outputs=[])
+    upload_btn.click(fn=upload_csv,
+                    inputs=[topic, file, labels, data_source], 
+                    outputs=[data_output_box, data_output_box, generate_qa_btn, retrieval_query])
+    #retrieval_query.render()
+    qa_output_box = gr.Textbox(label='Generated QA Pairs Status', visible=False)
+    generated_file = gr.File(label='Generated CSV', visible=False)
+    df_output = gr.Dataframe(label='Generated QA Pairs', visible=False, wrap=True)
+    generate_qa_btn.click(fn=generate_qa_pairs, 
+                          inputs=[topic, retrieval_query], 
+                          outputs=[qa_output_box, generated_file, df_output])
+
 demo.launch()
 
 # TODO - 
 # change output in gen_qa_pairs on status update
-# display generated qa pair df
+# display generated qa pair df -> make it run for tqdm(run doc)
 #
 # 1. data loader for all formats.
 # 2. TDQMs
 # 3. Option to choose different models in dashboard
 # 4. Set visibility to False for output_box, later True.
 # 5. Add data filtering (via class labels)
+# 6. 
